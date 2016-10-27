b99f20fbfd9f5b73d046d28cb5a2c49c350d07ba
Rainer Schuster <rainer@i-pol.com>
2016-10-27 19:43:30 +0700

    Added the clojure logo

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-UNAMESTR=$(UNAME)
-KERNEL_DIR=""
+KERNEL_SPEC="./resources/clj_jupyter"
 KERNEL=clojure
+BUILD_DIR=./build
 
 # Provide setup according to kernel-spec
 # https://jupyter-client.readthedocs.io/en/latest/kernels.html#kernel-specs
-if [[ "$UNAMESTR" == 'linux' ]]; then
-	KERNEL_DIR=~/.local/share/jupyter/kernels
-elif [[ "$UNAMESTR" == 'Darwin' ]]; then
-	KERNEL_DIR=~/Library/Jupyter/kernels
-fi
-
-TARGET_DIR=$KERNEL_DIR/$KERNEL
-
-mkdir -p $TARGET_DIR
-cp -f target/IClojure.jar $TARGET_DIR/IClojure.jar
-sed 's|HOME|'${HOME}'|' resources/clj_jupyter/kernel.json > $TARGET_DIR/kernel.json
+mkdir -p $BUILD_DIR
+cp -r $KERNEL_SPEC/* $BUILD_DIR
+cp -f target/IClojure.jar $BUILD_DIR/IClojure.jar
+sed 's|HOME|'${HOME}'|' $KERNEL_SPEC/kernel.json > $BUILD_DIR/kernel.json
+# do a sed later here to parse to install location and feed it in to 
+# the kernelspec, see sed about. Better yet, jupyter should provide an install hook
+jupyter kernelspec install --user --replace --name=$KERNEL $BUILD_DIR
 
